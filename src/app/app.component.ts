2ac6379dcaf371ff89fcb1d3e94572cfdb54d916
DanielFloria <daniflorialopez@gmail.com>
2022-09-07 22:31:25 +0200

    Calculadora completa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TA34-Calculadora';
+
+  operacion_entera: string = "";
+  numero_actual: string = "";
+
+  resultado: string = "";
+
+  clickC() {
+    this.operacion_entera = "";
+    this.numero_actual  = "";
+  }
+
+  clickCE() {
+    this.operacion_entera = "";
+  }
+
+  clickDel() {
+    this.numero_actual = this.numero_actual.substring(0, this.numero_actual.length - 1);
+  }
+
+  clickNumero(numero: string) {
+    if(this.numero_actual == "0" || this.numero_actual == "") {
+      this.numero_actual = numero;
+    } else {
+      this.numero_actual += numero;
+    }
+  }
+
+  clickPunto() {
+    if(!(this.numero_actual[this.numero_actual.length - 1] == ".")) {
+      this.numero_actual += ".";
+    }
+  }
+
+  clickOperacion(operador: string) {
+    if(this.numero_actual[this.numero_actual.length - 1] == ".") {
+      this.numero_actual = this.numero_actual.substring(0, this.numero_actual.length - 1);
+    }
+    this.operacion_entera = this.operacion_entera + this.numero_actual + operador;
+    this.numero_actual = "0";
+  }
+
+  clickIgual() {
+    if(this.numero_actual[this.numero_actual.length - 1] == ".") {
+      this.numero_actual = this.numero_actual.substring(0, this.numero_actual.length - 1);
+    }
+
+    if(this.operacion_entera[this.operacion_entera.length - 1].match(/\s/)) {
+      this.operacion_entera += this.numero_actual;
+      this.resultado = eval(this.operacion_entera);
+      this.numero_actual = this.resultado;
+    }
+  }
+
+  clickPositivoNegativo() {
+    if(this.numero_actual[0] == "-") {
+      this.numero_actual = this.numero_actual.substring(1,);
+    } else if(this.numero_actual !== "0" && this.numero_actual[0] !== "-" ){
+      this.numero_actual = "-" + this.numero_actual;
+    }
+  }
 }
